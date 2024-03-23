8e7df0056062331c021c689d2a78c6baa00328f8
Konoyukisao <163278889+Konoyukisao@users.noreply.github.com>
2024-03-23 10:12:47 +0700

    Homework5.py

--- Homework5.py
+++ Homework5.py
@@ -9,11 +9,11 @@ my_list[2] = "watermelon"; print(my_list)
 
 my_dict = {'hospital': 'больница', 'policeman': 'полицейский', 'food': 'еда', 'sword art online': 'мастера меча онлайн'}
 print(my_dict)
-mini_dict = ('hospital', 'policeman', 'food', 'sword art online') # mini_dict = (set(my_dict.keys())) - способ достать ключи, не выписывая вручную.
+mini_dict = ('hospital', 'policeman', 'food', 'sword art online') # mini_dict = (set(my_dict.keys())) - способ достать ключи, не выписывая вручную. Можно превращать как во множество, так и в список или кортеж.
 print('Я могу перевести следующие слова или сочетания слов с аглиского на русский:', mini_dict, 'выберите любое слово или сочитание.')
 user_word = input('')
 if user_word in mini_dict:
-    print(my_dict[user_word])
+    print('Перевод: ', my_dict[user_word])
 else:
     print('Выбранно не верное слово, или в слове допущенна ошибка.') # поигрались и хватит)
 my_dict['paper'] = 'бумага'
