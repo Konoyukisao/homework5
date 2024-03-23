3a8bdefe4a364e710d12d374235c0d1180c1c0ec
Konoyukisao <163278889+Konoyukisao@users.noreply.github.com>
2024-03-23 09:46:43 +0700

    Add files via upload

---
+++ Homework5.py
@@ -0,0 +1,20 @@
+my_list =['strawberry', 'banana', 'cherry', 'tomato', 'orange', 'aplle']
+print(my_list)
+print(my_list[0], my_list[-1], sep=', ')
+new_list = []
+for x in range(2, 5):
+    new_list.append(my_list[x])
+print(new_list)
+my_list[2] = "watermelon"; print(my_list)
+
+my_dict = {'hospital': 'больница', 'policeman': 'полицейский', 'food': 'еда', 'sword art online': 'мастера меча онлайн'}
+print(my_dict)
+mini_dict = ('hospital', 'policeman', 'food', 'sword art online')
+print('Я могу перевести следующие слова или сочетания слов с аглиского на русский:', mini_dict, 'выберите любое слово или сочитание.')
+user_word = input('')
+if user_word in mini_dict:
+    print(my_dict[user_word])
+else:
+    print('Выбранно не верное слово, или в слове допущенна ошибка.') # поигрались и хватит)
+my_dict['paper'] = 'бумага'
+print(my_dict)
